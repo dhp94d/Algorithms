dea7cbfbe8544500e9c2d3c5de84d960d4328943
donghyun <68668924+parkdit94@users.noreply.github.com>
2021-02-17 13:48:37 +0900

    백준 10872

---
+++ 백준/재귀/팩토리얼/10872.js
@@ -0,0 +1,16 @@
+//https://www.acmicpc.net/problem/10872
+
+var fs = require('fs');
+var input = fs.readFileSync('./1.txt').toString().trim().split(' ');
+let sum = 1;
+const factorial = (number) => {
+    if(number < 2){
+    }else{   
+        sum *= number;
+        factorial(number -1);
+    }
+}
+factorial(Number(input));
+console.log(sum);
+
+'/dev/stdin'
